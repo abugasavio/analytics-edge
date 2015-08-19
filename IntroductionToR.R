6eabcf04acf267ae2cdd2ebf479fc52018cee37d
Savio Abuga <savioabuga@gmail.com>
2015-08-19 11:18:58 +0300

    Vectors and DataFrames

--- IntroductionToR.R
+++ IntroductionToR.R
@@ -7,3 +7,26 @@ Country <- c("Brazil", "China", "India", "Switzerland", "USA")
 
 LifeExpectancy <- c(74, 76, 65, 83, 79)
 
+length(Country)
+
+length(LifeExpectancy)
+
+# Creating a Sequence
+seq(0, 100, 2)
+
+# Working with DataFrames
+CountryData <- data.frame(Country, LifeExpectancy)
+
+CountryData$Population <- c(199000, 1390000, 1240000, 7997, 318000)
+
+# Using rbind
+
+Country <- c("Austraria", "Greece")
+LifeExpectancy <- c(82, 81)
+Population <- c(23050, 11125)
+
+NewCountryData <- data.frame(Country, LifeExpectancy, Population)
+
+AllCountryData <- rbind(CountryData, NewCountryData)
+
+
