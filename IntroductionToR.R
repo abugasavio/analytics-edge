5e47fdfdb7e6280bb3586cf82b46aec0c620f88f
Savio Abuga <savioabuga@gmail.com>
2015-08-20 10:18:15 +0300

    working with plots, working with tapply

--- IntroductionToR.R
+++ IntroductionToR.R
@@ -1,5 +1,7 @@
 # Introduction to R in MITx: 15.071x The Analytics Edge Class
 # Author: Savio Abuga
+setwd("/Users/savioabuga/Documents/TheAnalyticEdge/")
+
 
 # Working with Vectors
 
@@ -29,4 +31,85 @@ NewCountryData <- data.frame(Country, LifeExpectancy, Population)
 
 AllCountryData <- rbind(CountryData, NewCountryData)
 
+# Reading DataFiles
 
+WHO <- read.csv("Data/WHO.csv")
+
+str(WHO)
+
+summary(WHO)
+
+# Subsetting
+
+WHO_Europe = subset(WHO, Region == "Europe")
+
+write.csv(WHO_Europe, "WHO_Europe.csv")
+
+ls()
+
+rm(WHO_Europe)
+
+ls()
+
+# DATA ANALYSIS - SUMMARY STATISTICS AND SCATTERPLOTS
+
+Under15
+
+WHO$Under15
+
+mean(WHO$Under15)
+
+sd(WHO$Under15)
+
+summary(WHO$Under15)
+
+which.min(WHO$Under15)
+
+WHO$Country[86]
+
+which.max(WHO$Under15)
+
+WHO$Country[124]
+
+plot(WHO$GNI, WHO$FertilityRate)
+
+outliers = subset(WHO, GNI > 10000 & FertilityRate>2.5)
+
+nrow(outliers)
+
+outliers[c("Country", "GNI", "FertilityRate")]
+
+# Quiz
+
+# What is the mean value of the "Over60" variable
+mean(WHO$Over60)
+
+# Which country has the smallest percentage of the population over 60?
+WHO$Country[which.min(WHO$Over60)]
+
+# Which country has the largest literacy rate?
+WHO$Country[which.max(WHO$LiteracyRate)]
+
+#VIDEO 6: DATA ANALYSIS - PLOTS AND SUMMARY TABLES
+
+hist(WHO$CellularSubscribers)
+
+boxplot(WHO$LifeExpectancy ~ WHO$Region)
+
+boxplot(WHO$LifeExpectancy ~ WHO$Region, xlab="", ylab="Life Expectancy", 
+        main="Life Expectancy of Countries by Region")
+
+table(WHO$Region)
+
+tapply(WHO$Over60, WHO$Region, mean)
+
+tapply(WHO$LiteracyRate, WHO$Region, mean, na.rm=TRUE)
+
+# QUIZ
+# Use the tapply function to find the average child mortality rate of countries
+# in each region.
+
+# Which region has the lowest average child mortality rate across all 
+# countries in that region?
+
+tapply(WHO$ChildMortality, WHO$Region, mean)
